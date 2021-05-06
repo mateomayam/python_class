d4385ae743b41fcc26bdf4760fd9b16ca5fc2ced
HeladiaSalgado <heladia@ccg.unam.mx>
2021-05-06 13:05:07 -0500

    Sugerencias de cambios al programa
    - Ajuste en nombre de las variables, para que sea explicito lo que guarda o para lo que se utiliza
    - No usar la misma variable sustituyendole el valor
    - Se corrigio que el programa respete el nombre de la secuencia y esa se use, no generar un nombre. Puede ser que no sea seq_1, sino ecoli  salm  ..
    - Se ajusto el encabezado
    - Se agrupo por bloques de código para que sea mas claro
    - los comentarios se ajustaron de acuerdo a PEP8, cortos, breves pero claros y siempre antes del bloque

--- src/fasta_sequences.py
+++ src/fasta_sequences.py
@@ -1,71 +1,78 @@
 '''
 NAME
-       [fasta_sequences].py
+       fasta_sequences.py
 
 VERSION
-        [1.0]
+        1.0
 
 AUTHOR
         Mateo Maya Martínez <[matteo.mayam@outlook.com>
 
 DESCRIPTION
-        [Este programa recibe como input un archivo .txt con secuencias de nucleótidos y con ellas crea un archivo .fasta]
 
+       Programa que permite convertir un archivo de secuencias raw a formato FastA
+       
 CATEGORY
-       [Genomic sequence.]
+       Genomic sequence.
 
 USAGE
-       [De uso bioinformático. Corre en python.
-       Este script corre desde el directorio src
-       El archivo .txt del que se leen las secuencias está ubicado en la ruta python_class/docs/4_dna_sequences.txt
-       El archivo .fasta se generará en el directorio src]
+
+       python fasta_sequences.py
+      
 
 OPTIONS
-       [Este programa no tiene opciones.]
+       Ninguna
+      
+INPUT FILE
+       4_dna_sequences.txt: El programa lee el archivo con este nombre y debe estar en el formato correcto
+                            El programa busca el archivo en la ruta relativa ../doc/4_dna_sequences.txt
+                               a partir de donde se corre el programa
+       Formato:
+              seq_1 = "ATCGTACGATCGATCGATCGCTAGACGTATCG"
+              seq_2 = "actgatcgacgatcgatcgatcacgact"
+              seq_3 = "ACTGAC-ACTGT-ACTGTA----CATGTG"
+                                    
+OUTPUT FILE
+       
+        dna_sequences.fasta: Nombre del archivo de salida en formato FastA
+        
+        Formato:
+              > seq_1
+              ATCGTACGATCGATCGATCGCTAGACGTATCG
+              > seq_2
+              ACTGATCGACGATCGATCGATCACGACT
+              > seq_3
+              ACTGACACTGTACTGTACATGTG          
 
-EXAMPLES
-       [Input: 4_dna_sequences.txt: seq_1 = "ATCGTACGATCGATCGATCGCTAGACGTATCG"
-                                    seq_2 = "actgatcgacgatcgatcgatcacgact"
-                                    seq_3 = "ACTGAC-ACTGT-ACTGTA----CATGTG"
-        Output: dna_sequences.fasta: > seq_1
-                                    ATCGTACGATCGATCGATCGCTAGACGTATCG
-                                    > seq_2
-                                    ACTGATCGACGATCGATCGATCACGACT
-                                    > seq_3
-                                    ACTGACACTGTACTGTACATGTG          ]
-
-LIGA DE GITHUB: [https://github.com/MateoMayaM/python_class/blob/master/src/fasta_sequences.py]
+SOURCE 
+       GitHub: https://github.com/MateoMayaM/python_class/blob/master/src/fasta_sequences.py
 
 '''
 
-# Abrir el archivo que contiene las secuencias.
-file = open("../docs/4_dna_sequences.txt")
+# Abrir y leer el archivo que contiene las secuencias
+input_file = open("../docs/4_dna_sequences.txt")
+input_file_lines = input_file.readlines()
+input_file.close()
 
-# Leer todas las lineas y guardarlas en una lista
-all_lines = file.readlines()
+# Crear el archivo de salida para guardar secuencias en formato FastA
+output_file = open("dna_sequences.fasta", "w")
 
-# Cerrar el archivo.
-file.close()
+# Procesar cada linea del archivo de entrada
+for line in input_file_lines:
+    line = line.split((" "))
+    seq_name = line[0]
+    sequence = (line [-1])
+    
+    # eliminar de la secuencia caracteres [- "]  que no son DNA
+    sequence = sequence.replace('-', '')
+    sequence = sequence.replace('"', '')
+       
+    # Formatear secuencias en un mismo estilo, todas a mayusculas
+    sequence = sequence.upper()
+    
+    # Guardar secuencias en formato FastA
+    output_file.write("> " + seq_name + "\n" + sequence)
 
-# Inicializar un contador.
-n = 1
+output_file.close()
 
-# Crear el archivo en el que se guardará el output, estará en el directorio docs, dentro del repositorio.
-new_file = open("dna_sequences.fasta", "w")
-
-# Procesar cade línea del archivo input y escribir el archivo output.
-for line in all_lines:
-    line = line.split((" ")) # Divide cada línea por ""
-    line = (line [-1]) # Quedarnos con el último elemento
-    line = line.replace('-', '') # Cambiar - por nada
-    line = line.replace('"', '') # Cambiar " por nada
-    line = line.upper() # Hacer todas las secuencias en letras mayúsculas
-    line = ("> seq_" + str(n) + "\n" + line) # Agregar el formato restante de un archivo .fasta
-    n += 1
-    print(line) # Imprimir el archivo
-    new_file.write(line) # Escribir el nuevo archivo .fasta
-
-# Cerrar el nuevo archivo.
-new_file.close()
-
-
+print ("The FastA file dna_sequences.fasta was created correctly\n")
